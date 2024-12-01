ee39e834d633026eb76567a2b06d2139b8be8d6e
TanyaSoni29 <tanu139sona@gmail.com>
2024-12-01 22:59:11 +0530

    Some correction and comments added

--- src/App.js
+++ src/App.js
@@ -7,51 +7,7 @@ import MovieList from "./components/Main/MovieList";
 import StarRatings from "./components/StarRatings";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+
 console.log(process.env.REACT_APP_OMDB_API);
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -85,7 +41,7 @@ export default function App() {
   }, [watched]);
 
   // for adding keypress and other event listener we use useRef hook this make the selection of the element easy and we have to write less code for that event listening
-
+  // always do practice with event listener so that it will be very helpful ....
   useEffect(() => {
     async function fetchMoviesData() {
       const controller = new AbortController(); // this is browser Api like fetch and it used to defend the race condition
