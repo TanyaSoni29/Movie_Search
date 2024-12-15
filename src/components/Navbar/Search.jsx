738d347af3fe5f3df422e6823b2de8145cf81d6c
TanyaSoni29 <tanu139sona@gmail.com>
2024-12-15 16:42:20 +0530

    event Listner and useRef use case study

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -2,10 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 
 function Search({ query, setQuery }) {
   const searchRef = useRef(null);
-
+  // adding eventListener for enter key press
   useEffect(() => {
-    searchRef.focus();
-  }, []);
+    function callBack(e) {
+      if (document.activeElement === searchRef.current) return; // this statement is for when inputElement is already in focus then don't run that event Listener
+      if (e.code === "Enter") {
+        searchRef.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callBack);
+
+    return () => document.addEventListener("keydown", callBack);
+  }, [setQuery]); // here need to pass setQuery inn dependency array because it is used inside the useEffect and it is a prop
   return (
     <input
       ref={searchRef}
